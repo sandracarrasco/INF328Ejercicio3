49489afd9e411748115166067b4d7dfbbbfe7765
SandraCalliconde <sady240299@gmail.com>
2021-04-20 23:30:40 -0400

    Add files via upload

---
+++ formulario.php
@@ -0,0 +1,6 @@
+<form method="GET" action="https://localhost:44355/WebForm1.aspx">
+    Nombre<input type="Text" name="nombre"/>
+	Apellido<input type="Text" name="apellido"/>
+	<input type="submit" name="Enviar"/>
+</form>
+	
